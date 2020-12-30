73e7b954d52ec48ee23e264e7bf344db19a8278f
Rodrigues, Michael C <mrodrigues@mitre.org>
2020-12-29 21:04:14 -0500

    progress on section 9

--- apps/section9/Quizzler/Quizzler-iOS13/ViewController.swift
+++ apps/section9/Quizzler/Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        [ "Four + Two is equal to Six.", "True" ],
+        [ "Five - Three is greater than One.", "True" ],
+        [ "Three + Eight is less than Ten.", "False" ]
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let questionInfo = quiz[questionNumber]
+        let userAnswer = sender.currentTitle
+        let actualAnswer = questionInfo[1]
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
+        } else {
+            print("Wrong!")
+        }
+        
+        questionNumber += 1
+        updateUI()
+    }
+    
+    func updateUI() {
+        let questionInfo = quiz[questionNumber]
+        let question = questionInfo[0]
+        questionLabel.text = question
+        progressBar.progress = Float(questionNumber) / Float(quiz.count)
+    }
+    
 }
 
