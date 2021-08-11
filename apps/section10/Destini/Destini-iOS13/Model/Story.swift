b3fb1298f43795306d197cedb927611e79904bac
Michael Rodrigues <rodrigues.mi@northeastern.edu>
2021-08-10 23:41:10 -0400

    Final code for section 10

--- apps/section10/Destini/Destini-iOS13/Model/Story.swift
+++ apps/section10/Destini/Destini-iOS13/Model/Story.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct Story {
+    var title = ""
+    var choice1 = ""
+    var choice2 = ""
+    var choice1Destination = 0
+    var choice2Destination = 0
+    
+    init(title: String, choice1: String, choice1Destination: Int, choice2: String, choice2Destination: Int) {
+        self.title = title
+        self.choice1 = choice1
+        self.choice2 = choice2
+        self.choice1Destination = choice1Destination
+        self.choice2Destination = choice2Destination
+    }
+}
